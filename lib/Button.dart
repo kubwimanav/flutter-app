8c9f4ffaf2d439f321022fd44ea592031250dc10
kubwimanav <kubwimanavincent2020@gmail.com>
2024-11-06 19:26:54 +0200

    button style

--- lib/Button.dart
+++ lib/Button.dart
@@ -7,19 +7,40 @@ class Button extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:Column(
+      body: Column(
         children: [
           Container(
-            child: const Text('hello this eleveted button',
-            style: TextStyle(
-              fontSize: 12,
-              fontStyle: FontStyle.normal
-            ),
+            child: const Text(
+              'hello this eleveted button',
+              style: TextStyle(fontSize: 12, fontStyle: FontStyle.normal),
             ),
           ),
-          
+          Container(
+              child: ElevatedButton(
+            onPressed: () {},
+            child: Text('Looks like a RaisedButton'),
+            style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.green),
+                padding: MaterialStatePropertyAll(EdgeInsets.all(10))),
+          )),
+          Container(
+            child:TextButton(
+              onPressed: () {
+                
+              },
+              child: Text('hello',
+              style:TextStyle(
+                fontSize: 10,
+                color: Colors.black
+              ),),
+              style:TextButton.styleFrom(
+                primary: Colors.green,
+                shape:const  BeveledRectangleBorder( borderRadius:BorderRadius.zero),
+                backgroundColor: Colors.green              )
+              ) ,
+          )
         ],
-      ) ,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Expand.dart';
+import 'package:flutter_application_1/Button.dart';
 void main() {
   runApp( const MaterialApp(
-      home: Expand(),
+      home: Button(),
   ));
 }
