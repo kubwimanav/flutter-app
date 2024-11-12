72c68bb4b50cc0af540ca782f90eb554941b545e
kubwimanav <kubwimanavincent2020@gmail.com>
2024-11-12 18:48:26 +0200

    homepage

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,22 +1,77 @@
 import 'package:flutter/material.dart';
 
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
-      padding: const EdgeInsets.all(20),
-      width: 200,
-      height: 100,
-      color: Colors.green,
-      child: Container(
-        child: const Text(
-          'hello my friend',
-          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+    return Column(
+      children: [
+       const Padding( padding: EdgeInsets.only(top:30), ),
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: const Icon(Icons.add, color: Colors.red,
+                ),
+              ),
+              Container(
+                child: const Icon(Icons.accessibility, color: Colors.amber,),
+              )
+            ],
+          ),
         ),
-      ),
+        Container(
+            child: ClipRRect(
+          borderRadius: BorderRadius.circular(50.0),
+          child: Image.asset(
+            'assets/pexels.jpg', 
+            width: 100.0,
+            height: 100.0,
+            fit: BoxFit.cover,
+          ),
+          ),
+        ),
+        Container( 
+        child:ElevatedButton (
+  
+          onPressed:(){}, 
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                     child:const Icon(
+                  Icons.account_box_outlined
+                ),
+                    ),
+                     Container
+                     (child: const Text('Items'))
+                    
+                  ],
+                ),
+              ),
+             Container(
+                    child: Row(
+                      children: [
+                        Container(
+                          child: const Icon(Icons.add_box),
+                        ),
+                        Container(child: const Text('Activity'))
+                      ],
+                    ),
+                  ),
+             
+            ],
+          )
+          ),
+          
+        )
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Button.dart';
+import 'package:flutter_application_1/Home.dart';
 void main() {
-  runApp( const MaterialApp(
-      home: Button(),
+  runApp( MaterialApp(
+    
+    initialRoute: '/home',
+    routes: {
+      '/home': (context) => const Home(),
+      '/button': (context) => const Button(),
+    },
   ));
 }
